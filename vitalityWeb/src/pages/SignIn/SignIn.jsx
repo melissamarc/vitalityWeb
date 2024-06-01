cf84d60a9b08f1cd97d401f318f3687b17ad93e7
unknown <melmarc1moreira@gmail.com>
2024-06-01 15:34:53 -0300

    modifiquei o css e comecei a adicionar as ondas

--- vitalityWeb/src/pages/SignIn/SignIn.jsx
+++ vitalityWeb/src/pages/SignIn/SignIn.jsx
@@ -1,72 +1,69 @@
-import { useState } from 'react'
-import React from 'react';
-import './SignIn.css';
-
+import { useState } from "react";
+import React from "react";
+import "./SignIn.css";
 
 function SignIn() {
-
-   const [usuario, setUsuario] = useState('');
-    const [senha, setSenha] = useState('');
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Adicionar a função e a lógica de dados
-        console.log('Usuário:', usuario);
-        console.log('Senha:', senha);
-    };
-
-
+  
   return (
-       
-
-    <div className='elementoPrincipal'>
-         
-                 <text> Login </text>
-
-                   <div className='inputs'> 
-                    <input 
-                        className='textCaixa'
-                        type="text" 
-                        id="usuario" 
-                        name="usuario" 
-                        placeholder="Email, telefone ou usuário"
-                       
-                        value={usuario} 
-                        onChange={(event) => setUsuario(event.target.value)} 
-                        required 
-                    />
-               
-                    <input 
-                        className='textCaixa'
-                        type="password" 
-                        id="senha" 
-                        name="senha" 
-                        placeholder="Senha" 
-               
-                        value={senha} 
-                        onChange={(event) => setSenha(event.target.value)} 
-                        required 
-                    />
-
-               </div>
-             <p> Esqueceu a senha? </p>
-                    <input className='entrarBotao' type="submit" value="Entrar" />
-
-                     <div className="button-container">
-            <a href="/" className="circle-button">
-                <img src="./facebook.png" alt="Imagem 1" className="circle-image" />
-            </a>
-            <a href="/" className="circle-button">
-                <img src="./google.png" alt="Imagem 2" className="circle-image" />
-            </a>
-        </div>
-
-        <p> Não tenho uma conta </p>
-                
-            
+<>
+     <div className="wave-background">
+      <div className="wave"></div>
     </div>
 
-  
+
+    <div className="elementoPrincipal">
+
+      <text> Login </text>
+
+      <div className="inputs">
+        <input
+          className="textCaixa"
+          type="text"
+          id="usuario"
+          name="usuario"
+          placeholder="Email, telefone ou usuário"
+          
+         
+        />
+
+        <input
+          className="textCaixa"
+          type="password"
+          id="senha"
+          name="senha"
+          placeholder="Senha"
+         
+         
+        />
+      </div>
+
+
+      <button className="esqueceuSenha"> Esqueceu a senha? </button>
+      {/* adicionar o href pra page de recuperação de senha */}
+
+
+     <input 
+     className="entrarBotao" 
+     type="submit"
+     value="Entrar" />  {/* adicionar o href pra pagina inicial */ } 
+
+      <div className="button-container">
+        <a href="/" className="circle-button">
+          {" "}
+          {/* adicionar a função p entrar com fb */}
+          <img src="./facebook.png" alt="Imagem 1" className="circle-image" />
+        </a>
+
+        <a href="/" className="circle-button">
+          <img src="./google.png" alt="Imagem 2" className="circle-image" />
+        </a>
+      </div>
+
+
+      <button className="botaoCadastro"> Não tenho uma conta </button>{" "}
+      {/* adicionar o href pra pagina de cadastro */}
+
+    </div> </>
   );
 }
 
