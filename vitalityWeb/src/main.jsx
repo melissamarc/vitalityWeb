92f4b98c5f2153e898d9fd0a1ba5eccb1b57f3ae
João Pedro Ribeiro de Assis <104910382+Joaoprassis@users.noreply.github.com>
2024-06-09 16:11:20 -0300

    Account
    Co-Authored-By: Melissa Marcielo Moreira <110108163+melissamarc@users.noreply.github.com>

--- vitalityWeb/src/main.jsx
+++ vitalityWeb/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SignIn from './pages/SignIn/SignIn.jsx'
+import account from './pages/SignIn/account.jsx'
 import App from './App.jsx'
 import './index.css'
 
